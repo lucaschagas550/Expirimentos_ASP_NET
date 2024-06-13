66380daf8a2a65ec7adeb1e0d3b8e90af8bfcbad
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2024-06-12 21:02:55 -0300

    feat: validacao forms e select search single

---
+++ Controllers/DataAnnotationController.cs
@@ -0,0 +1,131 @@
+﻿using DocumentFormat.OpenXml.InkML;
+using Microsoft.AspNetCore.Mvc;
+using TestTabelaResponivaBoostrap.Models;
+
+namespace TestTabelaResponivaBoostrap.Controllers
+{
+    public class DataAnnotationController : Controller
+    {
+        public DataAnnotationController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var alunos = CriarListaAlunos();
+
+            return View(alunos);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao,Ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+                return RedirectToAction(nameof(Index));
+
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, [Bind("Id,Nome,DataNascimento,Email,Avaliacao,Ativo")] Aluno aluno)
+        {
+            if (id != aluno.Id)
+            {
+                return NotFound();
+            }
+
+            ModelState.Remove("EmailConfirmacao");
+
+            if (ModelState.IsValid)
+                return RedirectToAction(nameof(Index));
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        private static List<Aluno> CriarListaAlunos()
+        {
+            return new List<Aluno>
+        {
+            new Aluno
+            {
+                Id = 1,
+                Nome = "João Silva",
+                DataNascimento = new DateTime(2000, 5, 15),
+                Email = "joao.silva@example.com",
+                EmailConfirmacao = "joao.silva@example.com",
+                Avaliacao = 4,
+                Ativo = true
+            },
+            new Aluno
+            {
+                Id = 2,
+                Nome = "Maria Souza",
+                DataNascimento = new DateTime(1999, 8, 20),
+                Email = "maria.souza@example.com",
+                EmailConfirmacao = "maria.souza@example.com",
+                Avaliacao = 5,
+                Ativo = true
+            },
+            new Aluno
+            {
+                Id = 3,
+                Nome = "Carlos Pereira",
+                DataNascimento = new DateTime(2001, 1, 10),
+                Email = "carlos.pereira@example.com",
+                EmailConfirmacao = "carlos.pereira@example.com",
+                Avaliacao = 3,
+                Ativo = true
+            },
+            new Aluno
+            {
+                Id = 4,
+                Nome = "Ana Lima",
+                DataNascimento = new DateTime(2000, 12, 25),
+                Email = "ana.lima@example.com",
+                EmailConfirmacao = "ana.lima@example.com",
+                Avaliacao = 4,
+                Ativo = true
+            },
+            new Aluno
+            {
+                Id = 5,
+                Nome = "Pedro Albuquerque",
+                DataNascimento = new DateTime(1998, 7, 5),
+                Email = "pedro.albuquerque@example.com",
+                EmailConfirmacao = "pedro.albuquerque@example.com",
+                Avaliacao = 2,
+                Ativo = true
+            },
+            new Aluno
+            {
+                Id = 6,
+                Nome = "Mariana Santos",
+                DataNascimento = new DateTime(2002, 9, 15),
+                Email = "mariana.santos@example.com",
+                EmailConfirmacao = "mariana.santos@example.com",
+                Avaliacao = 5,
+                Ativo = true
+            }
+        };
+
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestTabelaResponivaBoostrap.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DataType(DataType.DateTime, ErrorMessage = "O campo {0} está em formato incorreto")]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(60, ErrorMessage = "O campo {0} precisa ter no máximo {1} caracteres")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "O campo {0} está em formato inválido.")]
+        //[EmailAddress(ErrorMessage = "O campo {0} está em formato inválido.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Confirme o e-mail")]
+        [Compare("Email", ErrorMessage = "Os e-mails informados não são identicos")]
+        [NotMapped]
+        public string? EmailConfirmacao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(1, 5, ErrorMessage = "O campo {0} deve estar entre {1} e {2}")]
+        public int Avaliacao { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
