a4140dc4c2779f032fe5f9169b673bf901383ccc
Lucas <lucasandrade595@gmail.com>
2024-10-07 14:41:39 -0300

    feat: mascara de porcentagem infinita com decimal de duas casas

--- wwwroot/js/Views/masks.js
+++ wwwroot/js/Views/masks.js
@@ -3,4 +3,6 @@ $(document).ready(function () {
     $('#number').mask('000')
     $('.money').mask('000.000.000.000.000,00', { reverse: true });
     $('#time').mask('00:00');
+    //permite numeros infinitos antes da virgula e apenas duas casas decimais
+    $('.porcentagem-ilimitado-decimal-duas-casas').mask('#.##0,00', { reverse: true });
 });
