5492f8622cd28a90f2e0648585f2769601fbb39f
Lucas <lucasandrade595@gmail.com>
2024-10-07 17:08:47 -0300

    feat: editor de texto

---
+++ Controllers/EditorTextController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestTabelaResponivaBoostrap.Controllers.Base;
+using TestTabelaResponivaBoostrap.Models;
+
+namespace TestTabelaResponivaBoostrap.Controllers
+{
+    public class EditorTextController : MainController
+    {
+        public static EditorTexto textoPublic = new EditorTexto();
+
+        public EditorTextController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(textoPublic);
+        }
+
+        [HttpPost]
+        public IActionResult Create(EditorTexto editorTexto)
+        {
+            textoPublic.Content = editorTexto.Content;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/EditorTexto.cs
@@ -0,0 +1,17 @@
+﻿namespace TestTabelaResponivaBoostrap.Models
+{
+    public class EditorTexto
+    {
+        public EditorTexto(string content)
+        {
+            Content = content;
+        }
+
+        public EditorTexto()
+        {
+            
+        }
+
+        public string Content { get; set; } = string.Empty;
+    }
+}
